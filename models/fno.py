8570fc59fc1144f39bfaae93275a514257329b0f
Joshua <jlabasbas24@gmail.com>
2026-05-02 23:34:51 -0400

    spectral convolution

--- models/fno.py
+++ models/fno.py
@@ -3,6 +3,48 @@ from typing import Sequence
 import torch
 import torch.nn as nn
 
+class SpectralConv2d(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, modes1: int, modes2: int):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1
+        self.modes2 = modes2
+
+        scale = 1.0 / (in_channels * out_channels)
+        self.weights1 = nn.Parameter(
+            scale * torch.randn(in_channels, out_channels, modes1, modes2, dtype=torch.cfloat)
+        )
+        self.weights2 = nn.Parameter(
+            scale * torch.randn(in_channels, out_channels, modes1, modes2, dtype=torch.cfloat)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x_ft = torch.fft.rfft2(x)
+
+        m1 = min(self.modes1, x.size(-2))
+        m2 = min(self.modes2, x.size(-1) // 2 + 1)
+
+        out_ft = torch.zeros(
+            x.shape[0], self.out_channels, x.size(-2), x.size(-1) // 2 + 1,
+            dtype=torch.cfloat, device=x.device
+        )
+
+        out_ft[:, :, :m1, :m2] = torch.einsum(
+            "bixy, ioxy -> boxy",
+            x_ft[:, :, :m1, :m2],
+            self.weights1[:, :, :m1, :m2],
+        )
+
+        if x.size(-2) > m1:
+            out_ft[:, :, :m1, :m2] = torch.einsum(
+                "bixy, ioxy -> boxy",
+                x_ft[:, :, :m1, :m2],
+                self.weights2[:, :, :m1, :m2],
+            )
+
+        return torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+
 class FNO2d(nn.Module):
     def __init__(
             self, 
